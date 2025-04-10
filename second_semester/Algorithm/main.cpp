7efe8d5861d4dbd502dc385206067f1396daa93c
SIL1202 <167217913+SIL1202@users.noreply.github.com>
2025-04-10 15:50:07 +0800

    add: makefile, main.cpp
    finish main.cpp

--- second_semester/Algorithm/main.cpp
+++ second_semester/Algorithm/main.cpp
@@ -1,15 +1,25 @@
 #include "merge_sort.hpp"
 #include "quick_sort.hpp"
+#include <chrono>
 #include <iostream>
+using namespace std::chrono;
 
 int main() {
   int n;
   std::cin >> n;
+
+  auto start = high_resolution_clock().now();
   Quick quick(n);
-  quick.print();
+  auto end = high_resolution_clock().now();
+  auto duration_quick = duration_cast<microseconds>(end - start);
+  // quick.print();
+  std::cout << duration_quick.count() << std::endl;
 
-  std::cout << std::endl;
-
+  start = high_resolution_clock().now();
   Merge merge(n);
-  merge.print();
+  end = high_resolution_clock().now();
+  auto duration_merge = duration_cast<microseconds>(end - start);
+
+  std::cout << duration_quick.count() << std::endl;
+  // merge.print();
 }
