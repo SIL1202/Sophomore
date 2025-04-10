908d82c3ec4dbed194f8f16512e1b76317eadb66
SIL1202 <167217913+SIL1202@users.noreply.github.com>
2025-04-10 20:46:10 +0800

    change: input tests from n=2 to n=50 without caculate average.

--- second_semester/Algorithm/main.cpp
+++ second_semester/Algorithm/main.cpp
@@ -4,31 +4,31 @@
 #include <iostream>
 #include <vector>
 
+std::vector<int> generate_random_array(int);
+int main() {
+  int T = 1000;
+
+  for (int n = 2; n <= 100; ++n) {
+    std::chrono::nanoseconds total_quick{0}, total_insert{0};
+    std::vector<int> sample = generate_random_array(n);
+    Quick sorter(sample);
+    std::chrono::nanoseconds Quick = sorter.quick_duration();
+    std::chrono::nanoseconds Insertion = sorter.insertion_duration();
+
+    std::cout << "n = " << n << " | ";
+    std::cout << "QuickSort " << Quick.count() << " ns, ";
+    std::cout << "InsertionSort " << Insertion.count() << " ns\n";
+
+/*     if (Quick < Insertion)
+      break;
+  */ }
+
+return 0;
+}
+
 std::vector<int> generate_random_array(int n) {
   std::vector<int> arr(n);
   for (int &x : arr)
     x = rand(); // or rand() % 10000;
   return arr;
-}
-
-int main() {
-  int T = 1000;
-
-  for (int n = 2; n <= 50; ++n) {
-    std::chrono::nanoseconds total_quick{0}, total_insert{0};
-
-    for (int t = 0; t < T; ++t) {
-      std::vector<int> sample = generate_random_array(n);
-      Quick sorter(sample);
-      total_quick += sorter.quick_duration();
-      total_insert += sorter.insertion_duration();
-    }
-
-    std::cout << "n = " << n << " | ";
-    std::cout << "QuickSort avg: " << (total_quick / T).count() << " ns, ";
-    std::cout << "InsertionSort avg: " << (total_insert / T).count() << " ns\n";
-    if ((total_quick / T).count() < (total_insert / T).count())
-      exit(0);
-  }
-  return 0;
 }
