47720312661ee95b1b1de6392c7b97c80563ee00
SIL1202 <167217913+SIL1202@users.noreply.github.com>
2025-04-10 20:49:48 +0800

    feat: four inputSizes = {100, 1000, 10000, 100000}

--- second_semester/Algorithm/main.cpp
+++ second_semester/Algorithm/main.cpp
@@ -5,10 +5,24 @@
 #include <vector>
 
 std::vector<int> generate_random_array(int);
-int main() {
-  int T = 1000;
+void QuickSort_with_InsertionSort(std::vector<int> &);
+void MergeSort_with_InsertionSort(std::vector<int> &);
 
-  for (int n = 2; n <= 100; ++n) {
+int main() {
+  std::vector<int> inputSizes = {100, 1000, 10000, 100000};
+  QuickSort_with_InsertionSort(inputSizes);
+  return 0;
+}
+
+std::vector<int> generate_random_array(int n) {
+  std::vector<int> arr(n);
+  for (int &x : arr)
+    x = rand(); // or rand() % 10000;
+  return arr;
+}
+
+void QuickSort_with_InsertionSort(std::vector<int> &inputSizes) {
+  for (int n : inputSizes) {
     std::chrono::nanoseconds total_quick{0}, total_insert{0};
     std::vector<int> sample = generate_random_array(n);
     Quick sorter(sample);
@@ -18,17 +32,5 @@ int main() {
     std::cout << "n = " << n << " | ";
     std::cout << "QuickSort " << Quick.count() << " ns, ";
     std::cout << "InsertionSort " << Insertion.count() << " ns\n";
-
-/*     if (Quick < Insertion)
-      break;
-  */ }
-
-return 0;
-}
-
-std::vector<int> generate_random_array(int n) {
-  std::vector<int> arr(n);
-  for (int &x : arr)
-    x = rand(); // or rand() % 10000;
-  return arr;
+  }
 }
